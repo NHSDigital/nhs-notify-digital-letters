1437f9f7f69498be5b4a450c8520af8d282bf836
Ian Hodges <ian.hodges1@nhs.net>
2025-11-17 10:49:12 +0000

    CCM-12740: update unit test mocked values to differ from values on test object

--- lambdas/ttl-create-lambda/src/__tests__/apis/sqs-trigger-lambda.test.ts
+++ lambdas/ttl-create-lambda/src/__tests__/apis/sqs-trigger-lambda.test.ts
@@ -21,14 +21,14 @@ describe('createHandler', () => {
   const validItem: TtlItemEvent = {
     profileversion: '1.0.0',
     profilepublished: '2025-10',
-    id: '550e8400-e29b-41d4-a716-446655440001',
+    id: '999999999-9999-9999-9999-999999999999',
     specversion: '1.0',
     source: '/nhs/england/notify/production/primary/data-plane/digital-letters',
     subject:
       'customer/920fca11-596a-4eca-9c47-99f624614658/recipient/769acdd4-6a47-496f-999f-76a6fd2c3959',
     type: 'uk.nhs.notify.digital.letters.mesh.inbox.message.downloaded.v1',
-    time: '2023-06-20T12:00:00Z',
-    recordedtime: '2023-06-20T12:00:00.250Z',
+    time: '2000-01-02T02:00:00Z',
+    recordedtime: '2000-01-01T01:00:00Z',
     severitynumber: 2,
     traceparent: '00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01',
     datacontenttype: 'application/json',
