a88f075ae93809252dc8e8298661f7406bddb5a6
Ian Hodges <ian.hodges1@nhs.net>
2025-11-18 12:21:32 +0000

    CCM-13146 simple ttl insertion and deletion

---
+++ tests/playwright/digital-letters-component-tests/handle-ttl.component.spec.ts
@@ -0,0 +1,64 @@
+import { expect, test } from '@playwright/test';
+import { deleteTtl, putTtl } from 'helpers/dynamodb-helpers';
+import expectToPassEventually from 'helpers/expectations';
+import { v4 as uuidv4 } from 'uuid';
+
+test.describe('Digital Letters - Handle TTL', () => {
+  const baseEvent = {
+    profileversion: '1.0.0',
+    profilepublished: '2025-10',
+    specversion: '1.0',
+    source:
+      '/nhs/england/notify/production/primary/data-plane/digital-letters',
+    subject:
+      'customer/920fca11-596a-4eca-9c47-99f624614658/recipient/769acdd4-6a47-496f-999f-76a6fd2c3959',
+    type: 'uk.nhs.notify.digital.letters.mesh.inbox.message.downloaded.v1',
+    time: '2023-06-20T12:00:00Z',
+    recordedtime: '2023-06-20T12:00:00.250Z',
+    severitynumber: 2,
+    traceparent: '00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01',
+    datacontenttype: 'application/json',
+    dataschema:
+      'https://notify.nhs.uk/cloudevents/schemas/digital-letters/2025-10/digital-letter-base-data.schema.json',
+    dataschemaversion: '1.0',
+    severitytext: 'INFO',
+    data: {
+      messageReference: 'ref1',
+      senderId: 'sender1',
+    },
+  }
+
+
+  test('should handle withdrawn item', async () => {
+    const letterId = uuidv4();
+    const messageUri = `https://example.com/ttl/resource/${letterId}`;
+
+    const event = {
+      ...baseEvent,
+      id: letterId,
+      data: {
+        ...baseEvent.data,
+        messageUri,
+        'digital-letter-id': letterId,
+      }
+    }
+
+    const ttlItem = {
+      PK: messageUri,
+      SK: 'TTL',
+      dateOfExpiry: '2023-12-31#0',
+      event,
+      ttl: (Date.now() / 1000) + 3600,
+      withdrawn: true,
+    }
+
+
+    await expectToPassEventually(async () => {
+      const putResponseCode = await putTtl(ttlItem);
+      expect(putResponseCode).toBe(201);
+
+      const deleteResponseCode = await deleteTtl(messageUri);
+      expect(deleteResponseCode).toBe(200);
+    });
+  });
+});
--- tests/playwright/helpers/dynamodb-helpers.ts
+++ tests/playwright/helpers/dynamodb-helpers.ts
@@ -1,10 +1,12 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DeleteItemCommand, DeleteItemCommandOutput, DynamoDBClient, PutItemCommand, PutItemCommandOutput } from '@aws-sdk/client-dynamodb';
 import { QueryCommand, QueryCommandOutput } from '@aws-sdk/lib-dynamodb';
+import { marshall } from '@aws-sdk/util-dynamodb';
 import { REGION, TTL_TABLE_NAME } from 'constants/backend-constants';
+import { TtlDynamodbRecord } from 'utils';
 
 const dynamoDbClient = new DynamoDBClient({ region: REGION });
 
-async function getTtl(messageUri: string) {
+export async function getTtl(messageUri: string) {
   const params = {
     TableName: TTL_TABLE_NAME,
     KeyConditionExpression: `PK = :messageUri`,
@@ -18,4 +20,31 @@ async function getTtl(messageUri: string) {
   return Items ?? [];
 }
 
-export default getTtl;
+export async function putTtl(ttlItem: TtlDynamodbRecord) {
+  const params = {
+    TableName: TTL_TABLE_NAME,
+    Item: marshall(ttlItem),
+  };
+  const request = new PutItemCommand(params);
+  const output: PutItemCommandOutput = await dynamoDbClient.send(request);
+
+  return output.$metadata.httpStatusCode;
+}
+
+export async function deleteTtl(messageUri: string) {
+  const params = {
+    TableName: TTL_TABLE_NAME,
+    Key:{
+      PK: {
+        S: messageUri,
+      },
+      SK: {
+        S: 'TTL',
+      },
+    }
+  };
+  const request = new DeleteItemCommand(params);
+  const output : DeleteItemCommandOutput = await dynamoDbClient.send(request);
+
+  return output.$metadata.httpStatusCode;
+}
