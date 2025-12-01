429d12c3e8442a272f0f718a1eda4fe53e08080b
Gareth Allan <157592212+gareth-allan@users.noreply.github.com>
2025-12-01 14:59:21 +0000

    CCM-12896: Add temporary debug to pre.sh

--- infrastructure/terraform/components/dl/pre.sh
+++ infrastructure/terraform/components/dl/pre.sh
@@ -4,6 +4,11 @@
 # It ensures all Node.js dependencies are installed, generates any required dependencies,
 # and builds all Lambda functions in the workspace before Terraform provisions infrastructure.
 
+echo "Running pre-terraform script..."
+echo "Current directory: $(pwd)"
+echo "Listing files in current directory:"
+ls -la
+
 npm ci
 
 make generate
