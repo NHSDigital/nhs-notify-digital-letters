ef503182a6f1d688496d9537ad5ab6662898edd8
Gareth Allan <157592212+gareth-allan@users.noreply.github.com>
2025-12-08 16:25:00 +0000

    CCM-12896: Make acceptance tests generate dependencies

--- tests/playwright/digital-letters-component-tests/create-ttl.component.spec.ts
+++ tests/playwright/digital-letters-component-tests/create-ttl.component.spec.ts
@@ -14,7 +14,7 @@ test.describe('Digital Letters - Create TTL', () => {
         id: letterId,
         specversion: '1.0',
         source:
-          '/nhs/england/notify/production/primary/data-plane/digital-letters',
+          '/nhs/england/notify/production/primary/data-plane/digitalletters/mesh',
         subject:
           'customer/920fca11-596a-4eca-9c47-99f624614658/recipient/769acdd4-6a47-496f-999f-76a6fd2c3959',
         type: 'uk.nhs.notify.digital.letters.mesh.inbox.message.downloaded.v1',
