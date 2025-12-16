311969751fd6674be30ffd0251fd655366b6bd28
Ian Hodges <ian.hodges1@nhs.net>
2025-12-16 12:13:08 +0000

    CCM-12875: remove pdm-upload lambda

--- lambdas/pdm-upload-lambda/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { createHandler } from 'apis/sqs-trigger-lambda';
-import { createContainer } from 'container';
-
-export const handler = createHandler(createContainer());
-
-export default handler;
