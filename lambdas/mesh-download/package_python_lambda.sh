e31756917ab80eb88d1e8b1e2451ad4c4daed23b
lapenna-bjss <luca.lapenna@bjss.com>
2026-01-05 15:14:37 +0000

    CCM-12616: Update mesh download package script

--- lambdas/mesh-download/package_python_lambda.sh
+++ lambdas/mesh-download/package_python_lambda.sh
@@ -6,35 +6,19 @@ component_name="$1"
 rootdir=$(realpath "$(dirname "$0")/../..")
 source ${rootdir}/utils/get_version.sh
 
-VERSIONED_ZIP_NAME="NHSD.dl."${component_name}-"${POINT_VERSION}"".zip"
-
 dist_dir="${PWD}/target/dist"
-build_dir="${PWD}/target/python-build"
+rm -rf "${dist_dir}"
 mkdir -p "${dist_dir}"
-rm -rf "${build_dir}"
-mkdir -p "${build_dir}"
-rm -rf "${dist_dir}/${VERSIONED_ZIP_NAME}"
 
 # Extract internal (file://) and external dependencies from requirements.txt
 grep -E '^-e ' requirements.txt | sed 's|^-e ||' > target/internal_requirements.txt || true
 grep -vE '^-e ' requirements.txt > target/external_requirements.txt || true
 
 # Install external dependencies (from PyPI)
-pip install --platform manylinux2014_x86_64 --only-binary=:all: -r target/external_requirements.txt --target ${build_dir} --python-version 3.14 --implementation cp
+pip install --platform manylinux2014_x86_64 --only-binary=:all: -r target/external_requirements.txt --target ${dist_dir} --python-version 3.14 --implementation cp
 
 # Install internal dependencies (local packages)
-if [ -s target/internal_requirements.txt ]; then
-    while IFS= read -r dep; do
-        pip install "$dep" --target ${build_dir}
-    done < target/internal_requirements.txt
-fi
+pip install -r target/internal_requirements.txt --target ${dist_dir}
 
-for item in src/*; do
-    basename=$(basename "$item")
-    if [[ "$basename" != "__tests__" && "$basename" != "venv" && "$basename" != "__pycache__" ]]; then
-        cp -r "$item" "${build_dir}/"
-    fi
-done
-
-# Construct the build artefact
-cd ${build_dir} && zip -r "${dist_dir}/${VERSIONED_ZIP_NAME}" .
+# Bundle application code
+pip install . --no-deps --target ${dist_dir}
