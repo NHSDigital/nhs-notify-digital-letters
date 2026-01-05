e666aca86516a483a493bf01808461d83095d08c
Gareth Allan <157592212+gareth-allan@users.noreply.github.com>
2026-01-05 11:54:12 +0000

    CCM-12616: Don't zip Python lambda output when packaging

--- lambdas/mesh-poll/package_python_lambda.sh
+++ lambdas/mesh-poll/package_python_lambda.sh
@@ -6,27 +6,19 @@ component_name="$1"
 rootdir=$(realpath "$(dirname "$0")/../..")
 source ${rootdir}/utils/get_version.sh
 
-VERSIONED_ZIP_NAME="NHSD.dl."${component_name}-"${POINT_VERSION}"".zip"
-
 dist_dir="${PWD}/target/dist"
-build_dir="${PWD}/target/python-build"
+rm -rf "${dist_dir}"
 mkdir -p "${dist_dir}"
-rm -rf "${build_dir}"
-mkdir -p "${build_dir}"
-rm -rf "${dist_dir}/${VERSIONED_ZIP_NAME}"
 
 # Extract internal (file://) and external dependencies from requirements.txt
 grep -E '^-e ' requirements.txt | sed 's|^-e ||' > target/internal_requirements.txt || true
 grep -vE '^-e ' requirements.txt > target/external_requirements.txt || true
 
 # Install external dependencies (from PyPI)
-pip install --platform manylinux2014_x86_64 --only-binary=:all: -r target/external_requirements.txt --target ${build_dir} --python-version 3.14 --implementation cp
+pip install --platform manylinux2014_x86_64 --only-binary=:all: -r target/external_requirements.txt --target ${dist_dir} --python-version 3.14 --implementation cp
 
 # Install internal dependencies (local packages)
-pip install -r target/internal_requirements.txt --target ${build_dir}
+pip install -r target/internal_requirements.txt --target ${dist_dir}
 
 # Bundle application code
-pip install . --no-deps --target ${build_dir}
-
-# Construct the build artefact
-cd ${build_dir} && zip -r "${dist_dir}/${VERSIONED_ZIP_NAME}" .
+pip install . --no-deps --target ${dist_dir}
