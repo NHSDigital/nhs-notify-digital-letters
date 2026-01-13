08de8a17f80eb36f5188fcd5e9d67db676ba8353
Ian Hodges <ian.hodges1@nhs.net>
2026-01-13 11:09:28 +0000

    CCM-12614: component test updates

--- tests/playwright/digital-letters-component-tests/print-status-handler.component.spec.ts
+++ tests/playwright/digital-letters-component-tests/print-status-handler.component.spec.ts
@@ -1,45 +1,48 @@
 import { expect, test } from '@playwright/test';
+import { LetterEvent } from '@nhsdigital/nhs-notify-event-schemas-supplier-api/src/events/letter-events';
 import { ENV } from 'constants/backend-constants';
 import { getLogsFromCloudwatch } from 'helpers/cloudwatch-helpers';
 import eventPublisher from 'helpers/event-bus-helpers';
 import expectToPassEventually from 'helpers/expectations';
 import { v4 as uuidv4 } from 'uuid';
 
-import { LetterEvent } from '@nhsdigital/nhs-notify-event-schemas-supplier-api/src/events/letter-events';
-
-test.describe('Digital Letters - Create TTL', () => {
-  const baseLetterEvent = {
-    id: '550e8400-e29b-41d4-a716-446655440001',
-    specversion: '1.0',
-    source: '/data-plane/supplier-api/prod/update-status',
-    subject:
-      'letter-origin/digital-letters/letter/f47ac10b-58cc-4372-a567-0e02b2c3d479',
-    type: 'uk.nhs.notify.supplier-api.letter.ACCEPTED.v1',
-    dataschema:
-      'https://notify.nhs.uk/cloudevents/schemas/supplier-api/letter.ACCEPTED.1.0.0.schema.json',
-    dataschemaversion: '1.0.0',
-    time: '2023-06-20T12:00:00Z',
-    recordedtime: '2023-06-20T12:00:00.250Z',
-    severitynumber: 2,
-    traceparent: '00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01',
-    datacontenttype: 'application/json',
-    severitytext: 'INFO',
-    plane: 'data',
-    data: {
-      domainId: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
-      groupId: 'client_template',
-      origin: {
-        domain: 'letter-rendering',
-        event: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
-        source: '/data-plane/letter-rendering/prod/render-pdf',
-        subject:
-          'client/00f3b388-bbe9-41c9-9e76-052d37ee8988/letter-request/0o5Fs0EELR0fUjHjbCnEtdUwQe4_0o5Fs0EELR0fUjHjbCnEtdUwQe5',
-      },
-      specificationId: '1y3q9v1zzzz',
-      supplierId: 'supplier-1',
-      status: 'ACCEPTED',
+const baseLetterEvent = {
+  id: '550e8400-e29b-41d4-a716-446655440001',
+  specversion: '1.0',
+  source: '/data-plane/supplier-api/prod/update-status',
+  subject:
+    'letter-origin/digital-letters/letter/f47ac10b-58cc-4372-a567-0e02b2c3d479',
+  type: 'uk.nhs.notify.supplier-api.letter.ACCEPTED.v1',
+  dataschema:
+    'https://notify.nhs.uk/cloudevents/schemas/supplier-api/letter.ACCEPTED.1.0.0.schema.json',
+  dataschemaversion: '1.0.0',
+  time: '2023-06-20T12:00:00Z',
+  recordedtime: '2023-06-20T12:00:00.250Z',
+  severitynumber: 2,
+  traceparent: '00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01',
+  datacontenttype: 'application/json',
+  severitytext: 'INFO',
+  plane: 'data',
+  data: {
+    domainId: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
+    groupId: 'client_template',
+    origin: {
+      domain: 'letter-rendering',
+      event: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
+      source: '/data-plane/letter-rendering/prod/render-pdf',
+      subject:
+        'client/00f3b388-bbe9-41c9-9e76-052d37ee8988/letter-request/0o5Fs0EELR0fUjHjbCnEtdUwQe4_0o5Fs0EELR0fUjHjbCnEtdUwQe5',
     },
-  } as LetterEvent;
+    specificationId: '1y3q9v1zzzz',
+    supplierId: 'supplier-1',
+    status: 'ACCEPTED',
+  },
+} as LetterEvent;
+
+test.describe('Print status handler', () => {
+  test.beforeAll(async () => {
+    test.setTimeout(150_000);
+  });
 
   test('should create print.letter.transitioned ACCEPTED event for a letters.ACCEPTED event', async () => {
     const messageReference = uuidv4();
@@ -68,6 +71,6 @@ test.describe('Digital Letters - Create TTL', () => {
       );
 
       expect(eventLogEntry.length).toEqual(1);
-    });
+    }, 120 );
   });
 });
