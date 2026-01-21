cfa734d8a912c2ef5b68a85f8d8350ee4813c3d0
Ian Hodges <ian.hodges1@nhs.net>
2026-01-21 10:43:14 +0000

    CCM-13768: add file-safe s3 bucket

---
+++ infrastructure/terraform/components/dl/module_s3bucket_file_safe.tf
@@ -0,0 +1,84 @@
+module "s3bucket_file_safe" {
+  source = "https://github.com/NHSDigital/nhs-notify-shared-modules/releases/download/v2.0.24/terraform-s3bucket.zip"
+
+  name = "file-safe"
+
+  aws_account_id = var.aws_account_id
+  region         = var.region
+  project        = var.project
+  environment    = var.environment
+  component      = local.component
+
+  kms_key_arn = module.kms.key_arn
+
+  policy_documents = [data.aws_iam_policy_document.s3bucket_file_safe.json]
+
+  force_destroy = var.force_destroy
+
+  lifecycle_rules = [
+    {
+      enabled = true
+
+      expiration = {
+        days = "90"
+      }
+
+      noncurrent_version_transition = [
+        {
+          noncurrent_days = "30"
+          storage_class   = "STANDARD_IA"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        noncurrent_days = "90"
+      }
+
+      abort_incomplete_multipart_upload = {
+        days = "1"
+      }
+    }
+  ]
+}
+
+data "aws_iam_policy_document" "s3bucket_file_safe" {
+  statement {
+    sid    = "AllowManagedAccountsToList"
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      module.s3bucket_file_safe.arn,
+    ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${var.aws_account_id}:root"
+      ]
+    }
+  }
+
+  statement {
+    sid    = "AllowManagedAccountsToGet"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${module.s3bucket_file_safe.arn}/*",
+    ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${var.aws_account_id}:root"
+      ]
+    }
+  }
+}
