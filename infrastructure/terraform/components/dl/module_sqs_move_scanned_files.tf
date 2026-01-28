b39f8b7c3b711da55545c05587cb37265a43ffd4
Angel Pastor <angel.pastor1@nhs.net>
2026-01-28 17:28:59 +0000

    CMM-13767: remove condition

--- infrastructure/terraform/components/dl/module_sqs_move_scanned_files.tf
+++ infrastructure/terraform/components/dl/module_sqs_move_scanned_files.tf
@@ -34,11 +34,5 @@ data "aws_iam_policy_document" "sqs_move_scanned_files" {
     resources = [
       "arn:aws:sqs:${var.region}:${var.aws_account_id}:${local.csi}-move-scanned-files-queue"
     ]
-
-    condition {
-      test     = "ArnEquals"
-      variable = "aws:SourceArn"
-      values   = [ "arn:aws:guardduty:${var.region}:${var.aws_account_id}:malware-protection-plan/*" ]
-    }
   }
 }
