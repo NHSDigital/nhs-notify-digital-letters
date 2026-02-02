7c876f931f76579a89d0c67faa175ea962d2b750
lapenna-bjss <luca.lapenna@bjss.com>
2026-02-02 11:46:32 +0000

    CCM-13475: resolve fast-xml-parser security vulnerability

--- infrastructure/terraform/components/dl/module_sqs_core_notifier.tf
+++ infrastructure/terraform/components/dl/module_sqs_core_notifier.tf
@@ -38,7 +38,7 @@ data "aws_iam_policy_document" "sqs_inbound_event" {
     condition {
       test     = "ArnLike"
       variable = "aws:SourceArn"
-      values   = [ aws_cloudwatch_event_rule.pdm_resource_available.arn ]
+      values   = [aws_cloudwatch_event_rule.pdm_resource_available.arn]
     }
   }
 }
