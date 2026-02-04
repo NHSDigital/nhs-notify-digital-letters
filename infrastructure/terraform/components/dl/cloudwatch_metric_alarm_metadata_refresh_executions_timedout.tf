5a49fc894286d95f0974b4a166e3857c045b88a3
Gareth Allan <157592212+gareth-allan@users.noreply.github.com>
2026-02-04 14:19:02 +0000

    CCM-13295: Add a scheduled state machine to refresh Glue table metadata

---
+++ infrastructure/terraform/components/dl/cloudwatch_metric_alarm_metadata_refresh_executions_timedout.tf
@@ -0,0 +1,16 @@
+resource "aws_cloudwatch_metric_alarm" "metadata_refresh_executions_timedout" {
+  alarm_name          = "${local.csi}-metadata-refresh-executions-timedout"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "ExecutionsTimedOut"
+  namespace           = "AWS/States"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 1
+  alarm_description   = "This metric monitors step function execution timeouts"
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    StateMachineArn = aws_sfn_state_machine.metadata_refresh.arn
+  }
+}
