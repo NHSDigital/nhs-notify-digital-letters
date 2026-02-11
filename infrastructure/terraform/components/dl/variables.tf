2befc1a97c933a8527552c4080be36ebbd1ebc3d
simonlabarere <156111959+simonlabarere@users.noreply.github.com>
2026-02-11 14:45:57 +0000

    CCM-13475: Send print events (#179)
    * CCM-13475: Send print events
    
    * CCM-12614: add github package manager authentication
    
    * CCM-12614: add github package manager authentication
    
    * CCM-13475: Github token fix attempt
    
    * CCM-13475: Send print events
    
    * CCM-13475: Send print events
    
    * CCM-13475: Send print events
    
    * CCM-13475: Send print events
    
    * CCM-14347: Send Print config
    
    * CCM-14347: Send Print config
    
    * CCM-14347: Send Print config
    
    * CCM-14347: Send Print config
    
    * CCM-14347: Send Print config
    
    * CCM-14347: Send Print config
    
    * CCM-14347: Send Print config
    
    * CCM-14347: Send Print config
    
    * CCM-14347: Send Print config
    
    * CCM-13475: Remove package-lock.json file
    
    * CCM-13475: Test
    
    * CCM-13475: add input transformation
    
    * CCM-13475: pin @nhsdigital/nhs-notify-event-schemas-supplier-api to 1.0.6
    
    * CCM-13475: add campaignId to letter prepared events
    
    * CCM-13475: fix linter error
    
    * CCM-13475: address comments
    
    * CCM-13475: resolve fast-xml-parser security vulnerability
    
    * CCM-13475: Add eslint-disable sonarjs/hardcoded-secret-signatures in validate-private-key.test.ts
    
    * CCM-13475: Add max_receive_count to print-sender queue
    
    * CCM-13475: Revert package-lock.json changes in docs/
    
    * CCM-13475: remove unused constant
    
    ---------
    
    Co-authored-by: Ian Hodges <ian.hodges1@nhs.net>
    Co-authored-by: lapenna-bjss <luca.lapenna@bjss.com>
    Co-authored-by: lapenna-bjss <160487283+lapenna-bjss@users.noreply.github.com>

--- infrastructure/terraform/components/dl/variables.tf
+++ infrastructure/terraform/components/dl/variables.tf
@@ -181,6 +181,21 @@ variable "enable_pdm_mock" {
   default     = true
 }
 
+variable "aws_account_type" {
+  type        = string
+  description = "The AWS Account Type"
+}
+
+variable "eventpub_control_plane_bus_arn" {
+  type        = string
+  description = "Event publisher control plane"
+}
+
+variable "eventpub_data_plane_bus_arn" {
+  type        = string
+  description = "Event publisher data plane"
+}
+
 variable "report_scheduler_schedule" {
   type        = string
   description = "Schedule to trigger sender reports"
