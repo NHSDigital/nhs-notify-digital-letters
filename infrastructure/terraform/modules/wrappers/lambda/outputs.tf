f5758b8986d74c4ac2387be715947469bed4d7ed
lapenna-bjss <luca.lapenna@bjss.com>
2026-02-25 11:28:29 +0000

    CCM-14480: Fix outputs

--- infrastructure/terraform/modules/wrappers/lambda/outputs.tf
+++ infrastructure/terraform/modules/wrappers/lambda/outputs.tf
@@ -10,18 +10,3 @@ output "function_name" {
   description = "Name of the Lambda function"
   value       = module.lambda.function_name
 }
-
-output "role_arn" {
-  description = "ARN of the Lambda execution role"
-  value       = module.lambda.role_arn
-}
-
-output "role_name" {
-  description = "Name of the Lambda execution role"
-  value       = module.lambda.role_name
-}
-
-output "log_group_name" {
-  description = "Name of the CloudWatch log group"
-  value       = module.lambda.log_group_name
-}
