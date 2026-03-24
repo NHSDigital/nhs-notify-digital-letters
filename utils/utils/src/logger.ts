cdc610479dd3e6c82ceb1de891757954db114ad0
simonlabarere <simon.labarere1@nhs.net>
2026-03-24 16:17:46 +0000

    CCM-13675: Event code generation improvements

--- utils/utils/src/logger.ts
+++ utils/utils/src/logger.ts
@@ -4,10 +4,7 @@ const { combine, errors, json, timestamp } = winston.format;
 
 export const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
-  format: combine(
-    errors({ stack: true, cause: true }),
-    timestamp(),
-    json()),
+  format: combine(errors({ stack: true, cause: true }), timestamp(), json()),
   transports: [
     new winston.transports.Stream({
       stream: process.stdout,
