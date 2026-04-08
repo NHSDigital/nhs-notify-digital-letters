dac44d4964400534586cacc38d2cc84526568c82
Ian Hodges <ian.hodges1@nhs.net>
2026-04-08 14:39:08 +0100

    CCM-14615: temp coverage failure change 2

--- lambdas/pdm-poll-lambda/src/__tests__/apis/sqs-handler.test.ts
+++ lambdas/pdm-poll-lambda/src/__tests__/apis/sqs-handler.test.ts
@@ -35,44 +35,44 @@ describe('SQS Handler', () => {
   });
 
   describe('pdm.resource.submitted', () => {
-    it('should send pdm.resource.available event when the document is ready', async () => {
-      pdm.poll.mockResolvedValueOnce({
-        pdmAvailability: 'available',
-        nhsNumber: '9999999999',
-        odsCode: 'AB1234',
-      });
+    // it('should send pdm.resource.available event when the document is ready', async () => {
+    //   pdm.poll.mockResolvedValueOnce({
+    //     pdmAvailability: 'available',
+    //     nhsNumber: '9999999999',
+    //     odsCode: 'AB1234',
+    //   });
 
-      const response = await handler(recordEvent([pdmResourceSubmittedEvent]));
+    //   const response = await handler(recordEvent([pdmResourceSubmittedEvent]));
 
-      expect(eventPublisher.sendEvents).toHaveBeenCalledWith(
-        [
-          {
-            ...pdmResourceSubmittedEvent,
-            id: '550e8400-e29b-41d4-a716-446655440001',
-            time: '2023-06-20T12:00:00.250Z',
-            recordedtime: '2023-06-20T12:00:00.250Z',
-            dataschema:
-              'https://notify.nhs.uk/cloudevents/schemas/digital-letters/2025-10-draft/data/digital-letters-pdm-resource-available-data.schema.json',
-            type: 'uk.nhs.notify.digital.letters.pdm.resource.available.v1',
-            data: {
-              messageReference: pdmResourceSubmittedEvent.data.messageReference,
-              senderId: pdmResourceSubmittedEvent.data.senderId,
-              resourceId: pdmResourceSubmittedEvent.data.resourceId,
-              nhsNumber: '9999999999',
-              odsCode: 'AB1234',
-            },
-          },
-        ],
-        expect.any(Function),
-      );
-      expect(logger.info).toHaveBeenCalledWith(
-        'Received SQS Event of 1 record(s)',
-      );
-      expect(logger.info).toHaveBeenCalledWith(
-        '1 of 1 records processed successfully',
-      );
-      expect(response).toEqual({ batchItemFailures: [] });
-    });
+    //   expect(eventPublisher.sendEvents).toHaveBeenCalledWith(
+    //     [
+    //       {
+    //         ...pdmResourceSubmittedEvent,
+    //         id: '550e8400-e29b-41d4-a716-446655440001',
+    //         time: '2023-06-20T12:00:00.250Z',
+    //         recordedtime: '2023-06-20T12:00:00.250Z',
+    //         dataschema:
+    //           'https://notify.nhs.uk/cloudevents/schemas/digital-letters/2025-10-draft/data/digital-letters-pdm-resource-available-data.schema.json',
+    //         type: 'uk.nhs.notify.digital.letters.pdm.resource.available.v1',
+    //         data: {
+    //           messageReference: pdmResourceSubmittedEvent.data.messageReference,
+    //           senderId: pdmResourceSubmittedEvent.data.senderId,
+    //           resourceId: pdmResourceSubmittedEvent.data.resourceId,
+    //           nhsNumber: '9999999999',
+    //           odsCode: 'AB1234',
+    //         },
+    //       },
+    //     ],
+    //     expect.any(Function),
+    //   );
+    //   expect(logger.info).toHaveBeenCalledWith(
+    //     'Received SQS Event of 1 record(s)',
+    //   );
+    //   expect(logger.info).toHaveBeenCalledWith(
+    //     '1 of 1 records processed successfully',
+    //   );
+    //   expect(response).toEqual({ batchItemFailures: [] });
+    // });
 
     it('should send pdm.resource.unavailable event when the document is not ready', async () => {
       pdm.poll.mockResolvedValueOnce({
