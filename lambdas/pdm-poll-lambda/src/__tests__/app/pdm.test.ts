ace6d474ee3c9171a1127fdbf88c9b9b6ed2a78a
Ian Hodges <ian.hodges1@nhs.net>
2026-04-08 14:57:35 +0100

    CCM-14615: temp coverage failure change 2

--- lambdas/pdm-poll-lambda/src/__tests__/app/pdm.test.ts
+++ lambdas/pdm-poll-lambda/src/__tests__/app/pdm.test.ts
@@ -1,7 +1,6 @@
 import { mock } from 'jest-mock-extended';
 import { IPdmClient, Logger } from 'utils';
 import { Pdm, PdmDependencies } from 'app/pdm';
-import { pdmResourceSubmittedEvent } from '__tests__/test-data';
 
 const logger = mock<Logger>();
 const pdmClient = mock<IPdmClient>();
@@ -9,39 +8,6 @@ const validConfig = (): PdmDependencies => ({
   pdmClient,
   logger,
 });
-
-const availableResponse = {
-  resourceType: 'DocumentReference',
-  id: '4c5af7c3-ca21-31b8-924b-fa526db5379b',
-  meta: {
-    versionId: '1',
-    lastUpdated: '2025-12-10T09:00:47.068021Z',
-  },
-  status: 'current',
-  author: [
-    {
-      identifier: {
-        system: 'https://fhir.nhs.uk/Id/ods-organization-code',
-        value: 'Y05868',
-      },
-    },
-  ],
-  subject: {
-    identifier: {
-      system: 'https://fhir.nhs.uk/Id/nhs-number',
-      value: '9912003071',
-    },
-  },
-  content: [
-    {
-      attachment: {
-        contentType: 'application/pdf',
-        data: 'base64-encoded-pdf',
-        title: 'Dummy PDF',
-      },
-    },
-  ],
-};
 
 describe('Pdm', () => {
   describe('constructor', () => {
