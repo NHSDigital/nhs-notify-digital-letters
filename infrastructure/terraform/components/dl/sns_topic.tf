a175cc6b3687d12766977b263c005b247f3df860
Gareth Allan <157592212+gareth-allan@users.noreply.github.com>
2026-04-08 12:31:28 +0100

    CCM-15020: Permissions fixes

--- infrastructure/terraform/components/dl/sns_topic.tf
+++ infrastructure/terraform/components/dl/sns_topic.tf
@@ -13,8 +13,11 @@ data "aws_iam_policy_document" "sns_topic_policy_document" {
     effect = "Allow"
 
     principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${var.shared_infra_account_id}:root"]
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${var.shared_infra_account_id}:role/nhs-*-events-digital-letters-reporting",
+        "arn:aws:sts::${var.shared_infra_account_id}:assumed-role/nhs-*-events-digital-letters-reporting/*"
+      ]
     }
 
     actions = [
