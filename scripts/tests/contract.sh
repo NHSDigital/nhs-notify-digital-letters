8f2ddb41b9422ac2898954947ec04dd088eadcac
Gareth Allan <157592212+gareth-allan@users.noreply.github.com>
2026-04-14 11:07:34 +0100

    CCM-15633: Add contract tests to workflow

--- scripts/tests/contract.sh
+++ scripts/tests/contract.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 
 cd "$(git rev-parse --show-toplevel)"
 
+# Ensure we have the latest schema package matching our version specifier
+npm update @nhsdigital/nhs-notify-event-schemas-status-published
+
 npm run test:contract
